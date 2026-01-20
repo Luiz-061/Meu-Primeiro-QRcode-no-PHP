b7ecde636a928f7168e556d794ef2bfd40a71497
Luiz Pedro <musiclvp7@gmail.com>
2026-01-20 20:22:15 -0300

    Update QR code generation and output text

--- MeuPrimeiroQRCode/qrcode.php
+++ MeuPrimeiroQRCode/qrcode.php
@@ -1,12 +1,8 @@
 <?php 
-    $dados = [
-        'Nome' => 'Luiz',
-        'Idade' => 21,
-        'Profissão' => 'PHP Developer'
-    ];
-    $link = urldecode('https://publicapis.io/qr-code-api-api');
+    $data = urldecode('https://github.com/Luiz-061/'); //exemplo
+    $link = urldecode('https://publicapis.io/qr-code-api-api'); //Link da API
 
-    $url = "https://api.qrserver.com/v1/create-qr-code/?size=250x250&data={$link}"; 
+    $url = "https://api.qrserver.com/v1/create-qr-code/?size=250x250&data={$data}"; 
     
     //Mude a saída nesta variável $link, ou pode ser colocando este array associativo na linha 2 ou pode até criar uma outra variável se quiser.
 
@@ -15,5 +11,6 @@
 <img style="float: center;" src="<?= $url ?>" alt="QrCode">
 
 <p>
-   Qr code da API
+   Meu Primeiro QR code
 </p>
+
