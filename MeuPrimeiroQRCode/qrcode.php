1fbc3ee5e88d7cc1f0982d7a814a970a15520517
Luiz Pedro <musiclvp7@gmail.com>
2026-09-12 14:47:05 -0300

    Atualizar o qrcode.php

--- MeuPrimeiroQRCode/qrcode.php
+++ MeuPrimeiroQRCode/qrcode.php
@@ -2,13 +2,11 @@
     $data = urldecode('https://github.com/Luiz-061/'); //exemplo
     $link = urldecode('https://publicapis.io/qr-code-api-api'); //Link da API
 
-    $url = "https://api.qrserver.com/v1/create-qr-code/?size=250x250&data={$data}"; 
-    
-    //Mude a saída nesta variável $link, ou pode ser colocando este array associativo na linha 2 ou pode até criar uma outra variável se quiser.
+    $url = "https://api.qrserver.com/v1/create-qr-code/?size=250x250&data={$data}"; 
 
 ?>
 
-<img style="float: center;" src="<?= $url ?>" alt="QrCode">
+<img style="float: center;" src="<?= $url;?>" alt="QrCode">
 
 <p>
    Meu Primeiro QR code
